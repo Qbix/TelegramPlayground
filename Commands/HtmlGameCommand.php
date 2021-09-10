8d1fdc642085118e42d44e5291660a7347bc6657
Pavan Yogi <pavanyogi121@gmail.com>
2021-09-10 17:08:52 +0530

    add sendGame and add the game short name

--- Commands/HtmlGameCommand.php
+++ Commands/HtmlGameCommand.php
@@ -37,15 +37,20 @@ class HtmlGameCommand extends UserCommand
     public function execute(): ServerResponse
     {
         $message = $this->getMessage();            // Get Message object
-
         $chat_id = $message->getChat()->getId();   // Get the current Chat ID
+//        $data = [                                  // Set up the new message data
+//            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
+//            'text' => 'This is just a html game...', // Set message to send
+//        ];
+//        return Request::sendMessage($data);        // Send message!
 
-        $data = [                                  // Set up the new message data
-            'chat_id' => $chat_id,                 // Set Chat ID to send the message to
-            'text' => 'This is just a html game...', // Set message to send
+        $data = [
+            'chat_id' => $chat_id,
+            'game_short_name' => 'qbix_bot_game2' // change this game short name to as per your game short name
         ];
 
-        return Request::sendMessage($data);        // Send message!
+        return Request::sendGame($data);
+
     }
 
 
