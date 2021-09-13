18bce1e6b67d401051412bf6a26e8e4bcc493c92
Pavan Yogi <pavanyogi121@gmail.com>
2021-09-13 21:05:19 +0530

    Add custom play button

--- Commands/Custom/GenericmessageCommand.php
+++ Commands/Custom/GenericmessageCommand.php
@@ -21,6 +21,8 @@
 namespace Longman\TelegramBot\Commands\SystemCommands;
 
 use Longman\TelegramBot\Commands\SystemCommand;
+use Longman\TelegramBot\Entities\InlineKeyboard;
+use Longman\TelegramBot\Entities\InlineKeyboardButton;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request;
@@ -71,14 +73,29 @@ class GenericmessageCommand extends SystemCommand
         $chat_id = $message->getChat()->getId();
         $text = $message->getText();
 
-        if ($text === 'game') {
-            $data = [
-                'chat_id' => $chat_id,
-                'game_short_name' => 'qbix_bot_game2' // change this game short name to as per your game short name
-            ];
-            return Request::sendGame($data);
+
+        $result = Request::emptyResponse();
+
+        if($text !== 'game'){
+            return $result;
         }
 
-        return Request::emptyResponse();
+        $result = Request::sendGame([
+            'chat_id' => $chat_id,
+            'game_short_name' => 'qbix_bot_game2', // change this game short name to as per your game short name
+            'reply_markup' => new InlineKeyboard([
+                new InlineKeyboardButton([
+                    'text'=>"Play",
+                    'callback_game'=> 'Play qbix bot game'
+                ])
+            ]),
+        ]);
+
+        if ($result->isOk()) {
+            return $result;
+        }
+
+        throw new TelegramException($result->getDescription());
+
     }
 }
