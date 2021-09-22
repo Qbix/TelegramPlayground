39b975d607cf9a2cba2e7f78817587f283cf004c
Pavan Yogi <pavanyogi121@gmail.com>
2021-09-22 18:01:49 +0530

    revert GenericmessageCommand.php in the Custom folder

--- Commands/Custom/GenericmessageCommand.php
+++ Commands/Custom/GenericmessageCommand.php
@@ -21,8 +21,6 @@
 namespace Longman\TelegramBot\Commands\SystemCommands;
 
 use Longman\TelegramBot\Commands\SystemCommand;
-use Longman\TelegramBot\Entities\InlineKeyboard;
-use Longman\TelegramBot\Entities\InlineKeyboardButton;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request;
@@ -68,31 +66,15 @@ class GenericmessageCommand extends SystemCommand
      */
     public function execute(): ServerResponse
     {
-
         $message = $this->getMessage();
-        $chat_id = $message->getChat()->getId();
-        $text = $message->getText();
 
-        $result = Request::emptyResponse();
+        /**
+         * Handle any kind of message here
+         */
 
-        if($text === 'game'){
-            $result = Request::sendGame([
-                'chat_id' => $chat_id,
-                'game_short_name' => 'qbix_bot_game2', // change this game short name to as per your game short name
-                'reply_markup' => new InlineKeyboard([
-                    new InlineKeyboardButton([
-                        'text'=>"Play",
-                        'callback_game'=> 'Play qbix bot game'
-                    ])
-                ]),
-            ]);
-        }
+        $message_text = $message->getText(true);
 
-        if ($result->isOk()) {
-            return $result;
-        }
-
-        throw new TelegramException($result->getDescription());
+        return Request::emptyResponse();
 
     }
 }
