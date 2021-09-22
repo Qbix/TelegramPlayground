f92d87881cea40ad5dd8e597bcef7ff3c104458d
Pavan Yogi <pavanyogi121@gmail.com>
2021-09-22 15:47:21 +0530

    Add setGameScore call and add curl request to set game score

--- Commands/Custom/HighScoreHandler.php
+++ Commands/Custom/HighScoreHandler.php
@@ -1,15 +1,31 @@
 <?php
-require_once 'HighScore.php';
 
-if (isset($_GET['distance']) &&  isset($_GET['id'])) {
+// Load all configuration options
+/** @var array $config */
+$config = require __DIR__ . '/../../config.php';
 
-//    session_start();
+if (
+    isset($_GET['distance']) &&
+    isset($_GET['id']) &&
+    isset($_GET['user_id']) &&
+    isset($_GET['chat_id']) &&
+    isset($_GET['message_id'])
+) {
+    $highScore = (int)$_GET['distance'];
+    $user_id = (int)$_GET['user_id'];
+    $chat_id = (int)$_GET['chat_id'];
+    $message_id = (int)$_GET['message_id'];
 
-//    if(!in_array('qbix_'.$_GET['id'], $_SESSION)){
-//        return ;
-//    }
+    $url = 'https://api.telegram.org/bot' . $config['api_key'] . '/setGameScore?user_id=' . $user_id . '&score=' . $highScore . '&chat_id=' . $chat_id . '&message_id=' . $message_id . '&force=true';
 
-    $highScoreObj = new HighScore();
-    $highScoreObj->processHighScore($_GET['distance'], $_GET['id']);
+    $curl_handle = curl_init();
+    curl_setopt($curl_handle, CURLOPT_URL, $url);
+    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
+    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+    $buffer = curl_exec($curl_handle);
+    curl_close($curl_handle);
 
+    echo ($buffer) ? $buffer : "";
+    return;
 }
+
