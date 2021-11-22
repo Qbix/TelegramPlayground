e689b89cfa765e63d56fa815afdf29623100706e
Pavan Yogi <pavanyogi121@gmail.com>
2021-11-22 20:51:40 +0530

    Add send video functionality (/video command) to the telegram bot

---
+++ Commands/Custom/VideoCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Longman\TelegramBot\Commands\UserCommands;
+
+use Longman\TelegramBot\Commands\UserCommand;
+use Longman\TelegramBot\Entities\ServerResponse;
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Request;
+
+
+use Longman\TelegramBot\Conversation;
+
+
+class VideoCommand extends UserCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'video';                      // Your command's name
+
+    /**
+     * @var string
+     */
+    protected $description = 'A command for sending the video'; // Your command description
+
+    /**
+     * @var string
+     */
+    protected $usage = '/video';                    // Usage of your command
+
+    /**
+     * @var string
+     */
+    protected $version = '1.0.0';                  // Version of your command
+
+
+    /**
+     * Conversation Object
+     *
+     * @var Conversation
+     */
+    protected $conversation;
+
+    /**
+     * @var bool
+     */
+    protected $need_mysql = true;
+
+    /**
+     * @var bool
+     */
+    protected $private_only = false;
+
+    /**
+     * @return ServerResponse
+     * @throws TelegramException
+     */
+    public function execute(): ServerResponse
+    {
+        $message = $this->getMessage();
+        $chat = $message->getChat();
+        $chat_id = $chat->getId();
+
+        $data = [
+            'chat_id' => $chat_id,
+            'video' => 	'BAACAgUAAxkBAAEOl09hm7EvDxkEiWSBQh3XvilH5XNQfgAC3QMAAkaX4FQje_--aXA31yIE'
+        ];
+
+        $result = Request::sendVideo($data);
+
+        return $result;
+    }
+}
